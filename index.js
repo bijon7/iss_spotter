7e55ffade9dcc52c4a58ab2913c51f3dc4f749da
Zubaer <bijon7@yahoo.com>
2022-09-13 00:20:37 -0700

    Add fetchMyIP and fetchCoordsByIP

---
+++ index.js
@@ -0,0 +1,21 @@
+// Entry point to the application with the defined callbacks.
+const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:', ip);
+});
+
+fetchCoordsByIP('24.71.184.6', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:', coordinates);
+});
