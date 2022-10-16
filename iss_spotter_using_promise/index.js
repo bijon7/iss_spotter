60299b90a386e5f70549e28a9eebc445e7f7ebcb
Zubaer <bijon7@yahoo.com>
2022-10-15 23:31:30 -0700

    Add fetchISSFlyOverTimes function

--- iss_spotter_using_promise/index.js
+++ iss_spotter_using_promise/index.js
@@ -2,7 +2,9 @@
 
 const { fetchMyIP } = require('./iss_promised');
 const { fetchCoordsByIP } = require('./iss_promised');
+const { fetchISSFlyOverTimes } = require('./iss_promised');
 
 fetchMyIP()
   .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
   .then(body => console.log(body));
